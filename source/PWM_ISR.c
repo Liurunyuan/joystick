f73432786f78a5b7798c0a9798c2dbda5ee7bcbb
Simon <liurunyuan0802@163.com>
2018-07-30 21:33:59 +0800

    add more comments to function

--- source/PWM_ISR.c
+++ source/PWM_ISR.c
@@ -7,13 +7,27 @@
 
 struct BIT_AUX2_AN bit_aux2_an = {0};
 const Uint16 threshold[20] = {0};
-
+/**************************************************************
+ *Name:						Pwm_ISR_Thread
+ *Function:					PWM interrupt function
+ *Input:					none
+ *Output:					none
+ *Author:					Simon
+ *Date:						2018.6.10
+ **************************************************************/
 void Pwm_ISR_Thread(void)
 {
 	//TODO
 	AnalogValueInspect();
 }
-
+/**************************************************************
+ *Name:						AdcConversionUnStable
+ *Function:					判定模拟量多通道切换以及转换是否稳定
+ *Input:					none
+ *Output:					return 1表明本次不稳定 return 0表明转换已经稳定
+ *Author:					Simon
+ *Date:						2018.7.30
+ **************************************************************/
 int AdcConversionUnStable() {
 	static Uint16 count = 0;
 	++count;
@@ -26,7 +40,15 @@ int AdcConversionUnStable() {
 		return 1;
 	}
 }
-static int AnologChannelChange(int address)
+/**************************************************************
+ *Name:						AnologChannelChange
+ *Function:					模拟量多通道地址改变
+ *Input:					上次多通道模拟量地址值
+ *Output:					返回多通道模拟量地址值
+ *Author:					Simon
+ *Date:						2018.7.30
+ **************************************************************/
+int AnologChannelChange(int address)
 {
 
 	++address;
@@ -35,13 +57,26 @@ static int AnologChannelChange(int address)
 	}
 	return address;
 }
-
+/**************************************************************
+ *Name:						ReadChannelAdcValue
+ *Function:					读取DSP的ADC转换结果
+ *Input:					通道值
+ *Output:					none
+ *Author:					Simon
+ *Date:						2018.7.30
+ **************************************************************/
 void ReadChannelAdcValue(int index)
 {
 	bit_aux2_an.X[index] = CAL_ADCINB7;
 }
-
-/*switch analog channel, plus 1 every time*/
+/**************************************************************
+ *Name:						SwitchAnalogChannel
+ *Function:					切换多通道模拟量的地址值
+ *Input:					本次要设置的多通道模拟量的地址值
+ *Output:					none
+ *Author:					Simon
+ *Date:						2018.7.30
+ **************************************************************/
 void SwitchAnalogChannel(int address)
 {
 	/*
@@ -56,7 +91,14 @@ void SwitchAnalogChannel(int address)
 	SET_AD3K = address & 0x0003;
     SET_AD4K = address & 0x0004;
 }
-
+/**************************************************************
+ *Name:						AnalogValueInspect
+ *Function:					模拟量多通道巡检函数
+ *Input:					none
+ *Output:					none
+ *Author:					Simon
+ *Date:						2018.7.30
+ **************************************************************/
 void AnalogValueInspect(void)
 {
     static int address = 0;
