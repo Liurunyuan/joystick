409c34fc805c10fe6405a3727421a40699a36c4c
Simon <liurunyuan0802@163.com>
2018-08-09 21:55:34 +0800

    fitting function and least square method

---
+++ include/Filter_Alg.h
@@ -0,0 +1,22 @@
+#ifndef _FILTERALG_H
+#define _FILTERALG_H
+
+typedef struct _SumPara{
+	float sum_X;
+	float sum_Y;
+	float sum_Xpow2;
+	float sum_Xpow3;
+	float sum_Xpow4;
+	float sum_XY;
+	float sum_Xpow2Y;
+}SumPara;
+
+
+typedef struct _FuncPara{
+	float a;
+	float b;
+	float c;
+}FuncPara;
+
+
+#endif
---
+++ source/Filter_Alg.c
@@ -0,0 +1,72 @@
+#include <math.h>
+#include "Filter_Alg.h"
+
+
+#define SUMX 45
+#define SUMXPOW2 285
+#define SUMXPOW3 2025
+#define SUMXPOW4 415333
+#define SUMXPOW5 415333
+#define SUMXPOW6 415333
+
+SumPara sumPara = {
+	SUMX,
+	0,
+	SUMXPOW2,
+	SUMXPOW3,
+	SUMXPOW4,
+	0,
+	0
+};
+
+
+FuncPara funcPara = {0,0,0};
+
+void clearSum() {
+	sumPara.sum_XY = 0;
+	sumPara.sum_Xpow2Y = 0;
+	sumPara.sum_Y = 0;
+}
+
+void calFuncPara()
+{
+	float temp,temp0,temp1,temp2;
+
+	temp = 10.0 * (sumPara.sum_Xpow2 * sumPara.sum_Xpow4 -sumPara.sum_Xpow3*sumPara.sum_Xpow3)
+			-  sumPara.sum_X* (sumPara.sum_X * sumPara.sum_Xpow4 - sumPara.sum_Xpow2 * sumPara.sum_Xpow3);
+
+	temp0 = sumPara.sum_Y * (sumPara.sum_Xpow2 * sumPara.sum_Xpow4 - sumPara.sum_Xpow3*sumPara.sum_Xpow3)
+			- sumPara.sum_XY*(sumPara.sum_X*sumPara.sum_Xpow4 - sumPara.sum_Xpow2*sumPara.sum_Xpow3)
+			+ sumPara.sum_Xpow2Y*(sumPara.sum_X*sumPara.sum_Xpow3 - sumPara.sum_Xpow2*sumPara.sum_Xpow2);
+
+	temp1 = 10 * (sumPara.sum_XY*SUMXPOW4 - sumPara.sum_Xpow2Y*SUMXPOW3) - SUMX*(sumPara.sum_Y*SUMXPOW4 - SUMXPOW2*sumPara.sum_Xpow2Y)
+			+ SUMXPOW2*(sumPara.sum_Y*SUMXPOW3 - sumPara.sum_XY*SUMXPOW2);
+
+	temp2 = 10 * (SUMXPOW2*sumPara.sum_Xpow2Y - sumPara.sum_XY*SUMXPOW3)
+			- sumPara.sum_X*(sumPara.sum_X * sumPara.sum_Xpow2Y - sumPara.sum_Y*SUMXPOW3)
+			+ SUMXPOW2*(SUMX*sumPara.sum_XY - sumPara.sum_Y*SUMXPOW2);
+
+	funcPara.c = temp0/temp;
+	funcPara.b = temp1/temp;
+	funcPara.a = temp2/temp;
+
+}
+void deal(int y)
+{
+	static int count = 0;
+
+	if(count == 0)
+	{
+		clearSum();
+	}
+
+	sumPara.sum_XY += count*y;
+	sumPara.sum_Xpow2Y += count*count*y;
+	sumPara.sum_Y += y;
+
+	if(count ==9)
+	{
+		calFuncPara();
+	}
+	count++;
+}
