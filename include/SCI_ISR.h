4b862e22eb45c609440c3e434d711780c5d45ee4
Simon <liurunyuan0802@163.com>
2018-08-11 16:57:43 +0800

    add RS422 unpack function

--- include/SCI_ISR.h
+++ include/SCI_ISR.h
@@ -1,5 +1,26 @@
+#ifndef _SCI_ISR_H
+#define _SCI_ISR_H
 
-
-
+#define MAXQSIZE 128
 #define RXBUGLEN (16)
 typedef void (*functionMsgCodeUnpack)(int a, int b,int c);
+
+#define HEAD 0x55
+#define TAIL 0xAA
+
+typedef struct _RS422RXQUE{
+	int rxBuff[MAXQSIZE];
+	int front;
+	int rear;
+}RS422RXQUE;
+
+enum {
+	FindHead,
+	CheckLength,
+	CheckTail,
+	CheckCRC,
+	Unpack,
+	UpdateHead
+};
+
+#endif
--- source/SCI_ISR.c
+++ source/SCI_ISR.c
@@ -1,8 +1,11 @@
+#include "DSP2833x_Device.h"     // DSP2833x Headerfile Include File
+#include "DSP2833x_Examples.h"   // DSP2833x Examples Include File
 #include "SCI_ISR.h"
 
 
 /***********globle variable define here***************/
 int recievechar[RXBUGLEN]={0};
+RS422RXQUE gRS422RxQue = {0};
 
 static void MsgStatusUnpack(int a, int b, int c)
 {
@@ -20,18 +23,123 @@ const functionMsgCodeUnpack msgInterface[] =
 		0
 };
 
+int EnQueue(int e){
+	if((gRS422RxQue.rear + 1)%MAXQSIZE == gRS422RxQue.front)
+		return 0;
 
-/*************************************/
-void RS422A_recieve(void)
+	gRS422RxQue.rxBuff[gRS422RxQue.rear] = e;
+	gRS422RxQue.rear = (gRS422RxQue.rear + 1)%MAXQSIZE;
+	return 1;
+}
+//head:0x55
+//length:0xXX
+//data:.....
+//CRC:0xXX
+//tail:0xAA
+int DeQueue()
 {
-	int index;
-	for(index = 0; index < RXBUGLEN; ++index)
-	{
-		recievechar[index] = 1;
+	if(gRS422RxQue.front == gRS422RxQue.rear)
+		return 0;
 
-		if(1>=1)
-		{
-
+	gRS422RxQue.front = (gRS422RxQue.front + 1)%MAXQSIZE;
+	return 1;
+}
+/*************************************/
+void RS422A_receive(void)
+{
+	//当接收fifo不为空时
+	while(1){
+		if(EnQueue(ScicRegs.SCIRXBUF.all) == 0){
+			//TODO update error msg
 		}
 	}
 }
+
+void UnpackRS422A()
+{
+	static int status = FindHead;
+	switch (status)
+	{
+	case FindHead:
+		while(gRS422RxQue.rxBuff[gRS422RxQue.front] != HEAD){
+			if(DeQueue() == 0){
+				//没有包可以解，接收缓冲区为空
+				return;
+			}
+		}
+		status = CheckLength;
+	case CheckLength:
+		if((gRS422RxQue.front + gRS422RxQue.rxBuff[gRS422RxQue.front + 1] + 3) < gRS422RxQue.rear)//接收缓冲区内数据长度大于一整包的长度
+		{
+			status = CheckTail;
+		}
+		else
+		{
+			break;
+		}
+	case CheckTail:
+		if(gRS422RxQue.rxBuff[gRS422RxQue.front + gRS422RxQue.rxBuff[gRS422RxQue.front + 1] + 3] == TAIL)
+		{
+			status = CheckCRC;
+		}
+		else
+		{
+			gRS422RxQue.front += 1;
+			status = FindHead;
+			break;
+		}
+	case CheckCRC:
+		// TODO CRC校验
+		if(1){
+			status = Unpack;
+		}
+		else
+		{
+			gRS422RxQue.front += 1;
+			status = FindHead;
+			break;
+		}
+	case Unpack:
+		//TODO
+		status = UpdateHead;
+	case UpdateHead:
+		gRS422RxQue.front = gRS422RxQue.front + gRS422RxQue.rxBuff[gRS422RxQue.front + 1] + 4;
+		status = FindHead;
+		break;
+	default:
+		status = FindHead;
+		break;
+	}
+	/*
+	while(gRS422RxQue.rxBuff[gRS422RxQue.front] != HEAD){
+		if(DeQueue() == 0){
+			//没有包可以解，接收缓冲区为空
+			return;
+		}
+	}
+
+	if((gRS422RxQue.front + gRS422RxQue.rxBuff[gRS422RxQue.front + 1] + 3) < gRS422RxQue.rear)//接收缓冲区内数据长度大于一整包的长度
+	{
+		if(gRS422RxQue.rxBuff[gRS422RxQue.front + gRS422RxQue.rxBuff[gRS422RxQue.front + 1] + 3] == TAIL){
+
+		}
+	}
+	else
+	{
+
+	}
+	*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
