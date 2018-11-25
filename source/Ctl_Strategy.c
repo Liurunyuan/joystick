2c0b6b25a2dbf4eb6f8599cd55db9383153334cf
Simon <unknown@example.com>
2018-11-25 21:30:49 +0800

    Add the interface of control strategy

--- source/Ctl_Strategy.c
+++ source/Ctl_Strategy.c
@@ -2,3 +2,42 @@
 #include "DSP2833x_Examples.h"   // DSP2833x Examples Include File
 #include "public.h"
 #include "Ctl_Strategy.h"
+#include "GlobalVarAndFunc.h"
+
+
+void CalculateSpringForce(void) {
+
+}
+
+void CalculateDampForce(void) {
+
+}
+
+void CalculateTargetAcc(void) {
+
+}
+
+void CalculateTargetSpeed(void){
+
+}
+
+void CalculateTargeDisplace(void) {
+
+}
+
+void UpdateAccErr(void) {
+
+}
+
+void UpdateSpeedErr(void) {
+
+}
+
+void UpdateDisplacementErr(void) {
+
+}
+
+
+void PidProcess(void){
+
+}
